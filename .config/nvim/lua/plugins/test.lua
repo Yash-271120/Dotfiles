b70fa7f16ed179b9b06b6d8408f8e574f91fb63d
Yash-271120 <yashpatil.yp2711@gmail.com>
2025-11-07 10:19:50 +0000

    update: upgrade version and plugins

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -3,6 +3,7 @@ return {
   {"rouge8/neotest-rust"},
   {
     "nvim-neotest/neotest",
+    dependencies = { "nvim-neotest/nvim-nio" },
     opts = { adapters = { "neotest-jest", "neotest-rust" } },
   },
 }
