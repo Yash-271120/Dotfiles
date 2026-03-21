2ae2c62905d8b28129c1d83b7f2bd5feecfc2ccc
Yash-271120 <yashpatil.yp2711@gmail.com>
2026-03-21 12:54:48 +0000

    new minimal nvim

--- .config/nvim/lua/plugins/test.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "nvim-neotest/neotest-jest" },
-  {"rouge8/neotest-rust"},
-  {
-    "nvim-neotest/neotest",
-    dependencies = { "nvim-neotest/nvim-nio" },
-    opts = { adapters = { "neotest-jest", "neotest-rust" } },
-  },
-}
